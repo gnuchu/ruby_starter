84e8f2e80ef68540073b871709d8c54d60764ab6
John Griffiths <gnuchu@gmail.com>
2019-10-25 12:54:16 +0100

    add command line utility into starter pack

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 ruby '2.6.4'
 
-gem 'oauth'
+gem 'command_line'
--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'benchmark'
+
+require 'command_line/global'
